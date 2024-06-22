9ab8ef416962bbc3c751d99cabf129973b555cd1
murtajiz1231 <noordark00@gmail.com>
2024-06-22 23:02:59 +0500

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,270 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  nextjs,
+  express
+  
+ 
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+ 
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Next Js Developer",
+    icon: mobile,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Tailwind and Bootstrap",
+    company_name: "HTML and CSS",
+    icon: html,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Proficient in HTML: I have a strong understanding of HTML, allowing me to create well-structured, semantic web pages that ensure accessibility and SEO optimization.",
+      "Expert in CSS: I possess advanced knowledge of CSS, enabling me to design visually appealing and responsive layouts, animations, and user interfaces that enhance user experience.",
+      "Tailwind CSS Specialist: I am skilled in using Tailwind CSS, which allows me to rapidly build custom designs with utility-first classes, ensuring a streamlined and efficient development process.",
+      "Bootstrap Professional: I have extensive experience with Bootstrap, leveraging its powerful grid system, pre-styled components, and responsive utilities to create robust, mobile-first web applications."
+    ],
+  },
+  {
+    title: "React Developer",
+    company_name: "FrontEnd ",
+    icon: reactjs,
+    iconBg: "#E6DEDD",
+    date: "Jan 2021 - Feb 2022",
+    points: [
+      "Component-Based Architecture: I am proficient in building reusable and modular components in React, enabling efficient and maintainable code that enhances the scalability of web applications.",
+      "State and Props Management: I have a deep understanding of state and props management in React, allowing me to effectively handle data flow and ensure seamless interaction between components.",
+      "Hooks and Functional Components: I am experienced in utilizing React Hooks and functional components to create dynamic and responsive user interfaces, improving code readability and performance.",
+      "Integration with Modern Tools: I am skilled in integrating React with modern development tools and libraries, such as Redux for state management, React Router for navigation, and Material-UI for styling, to build comprehensive and robust applications."
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "BackEnd and DataBase",
+    icon: express,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Proficient in Node.js: I have a strong command of Node.js, enabling me to build efficient, scalable, and high-performance server-side applications with a focus on asynchronous programming and event-driven architecture.",
+      "Expert in Express.js: I am well-versed in using Express.js, the flexible and minimal web framework for Node.js, to create robust RESTful APIs, handle routing, and manage middleware for streamlined web application development.",
+      "Database Management: I possess comprehensive knowledge of database management, including both SQL (e.g., MySQL, PostgreSQL) and NoSQL (e.g., MongoDB) databases, allowing me to design, query, and optimize database schemas for efficient data storage and retrieval.",
+      "Seamless Integration: I am skilled in integrating Node.js and Express.js with various databases, ensuring secure, reliable, and performant data handling within web applications, and implementing best practices for data validation, authentication, and error handling."
+    ],
+  },
+  {
+    title: "Next JS Developer",
+    company_name: "Present",
+    icon: nextjs,
+    iconBg: "#E6DEDD",
+    date: "Jun 2024 - Present",
+    points: [
+      "Server-Side Rendering (SSR) and Static Site Generation (SSG): I have a strong understanding of Next.js features like SSR and SSG, enabling me to build high-performance, SEO-friendly web applications that deliver content efficiently.",
+      "Routing and Navigation: I am proficient in using Next.js's built-in routing system, which simplifies the creation of dynamic and nested routes, ensuring seamless navigation and improved user experience.",
+      "API Routes and Integration: I possess expertise in creating API routes in Next.js, allowing me to build robust backend services and integrate them seamlessly with the frontend, providing a full-stack development experience.",
+      "Optimized Performance and Deployment: I am skilled in optimizing Next.js applications for performance, utilizing techniques such as code-splitting, lazy loading, and image optimization, and I am experienced in deploying Next.js projects to platforms like Vercel for efficient and scalable hosting."
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Coming Soon",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Coming Soon",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Coming Soon",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "Pending",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
